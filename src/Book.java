7aba893d2098dae806dd35d43554a78889b6fec8
Villa1123 <vich1001@stud.ek.dk>
2026-02-19 10:11:06 +0100

    Book

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,11 @@
-public class Book {
-}
+public class Books extends Item {
+
+        public Books(String title, int loanDays) {
+            super(title, loanDays);
+        }
+        @Override
+        public String toString() {
+            return "Book 📚 | " + super.toString();
+        }
+        }
+
