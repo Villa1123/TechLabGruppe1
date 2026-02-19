4dbb9df4609bdb8a217594b58791139128f6cd29
AmandaHult98 <amhu1000@stud.ek.dk>
2026-02-19 10:16:16 +0100

    Commit

--- src/Electronic.java
+++ src/Electronic.java
@@ -1,2 +1,15 @@
-public class Electronic {
+public class Electronic extends Item {
+
+    private String author;
+
+    public Electronic(String name, int loanDays, String author) {
+        super(name, loanDays);
+        this.author = author;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Electronic | " + super.toString() + " | " + author;
+    }
 }
