c2b885bd489d8b6123dde7d4997625fc7d0a366e
Villa1123 <vich1001@stud.ek.dk>
2026-02-19 10:06:48 +0100

    Hej

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+public abstract class Item {
 
-public class Item {
-}
+        private String title;
+        private int loanDays;
+
+        // Constructor
+        public Item(String title, int loanDay) {
+            this.title = title;
+            this.loanDays = loanDays;
+        }
+
+        // Getter
+        public String getTitle() {
+            return title;
+        }
+
+        public int getLoanDays() {
+            return loanDays;
+        }
+
+        // Setter
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public void setLoanDays(int loanDays) {
+            this.loanDays = loanDays;
+        }
+
+        // Polymorphic method (VERY important)
+        public abstract void useProduct();
+
+        @Override
+        public String toString() {
+            return "Product: " + title + " | Loan Days: " + loanDays;
+        }
+    }
+
+
