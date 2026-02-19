62695968d99ce708dced4239b6a0ce06f6e492e0
aishadirie <aish0078@stud.ek.dk>
2026-02-19 11:32:29 +0100

    video

--- src/Video.java
+++ src/Video.java
@@ -1,2 +1,17 @@
-public class Video {
-}
+  public class Video extends Item {
+
+     private int duration;
+
+
+     public Video(String title, int loanDays, int duration) {
+         super(title, loanDays);
+         this.duration = duration;
+     }
+
+     @Override
+     public String toString() {
+         return "Video| " + super.toString() + "|Duration" + duration;
+
+
+     }
+ }
