3294a4c60dbf319bfa3fca8c45337b2271b05357
AmandaHult98 <amhu1000@stud.ek.dk>
2026-02-19 11:04:23 +0100

    Commit

--- src/TechLab.java
+++ src/TechLab.java
@@ -1,2 +1,117 @@
+import java.util.Scanner;
+
 public class TechLab {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int count = askForCount(sc);
+
+        Item[] itemArray = createItemArray(sc, count);
+
+        printItem(itemArray);
+
+        sortByName(itemArray);
+
+        System.out.println("\n--- Sorted by Title ---");
+        printItem(itemArray);
+
+        sc.close();
+
+    }
+
+    // Ask user for number of items
+    public static int askForCount(Scanner sc) {
+
+        int count = 0;
+
+        while (true) {
+            System.out.print("How many furniture items? ");
+
+            if (sc.hasNextInt()) {
+                count = sc.nextInt();
+                sc.nextLine();
+
+                if (count > 0) break;
+                else System.out.println("Enter a positive number!");
+            } else {
+                System.out.println("Invalid number!");
+                sc.nextLine();
+            }
+        }
+
+        return count;
+    }
+
+
+    public static Item[] createItemArray(Scanner sc, int count) {
+
+        Item[] techLab = new Item[count];
+
+        for (int i = 0; i < count; i++) {
+
+            System.out.println("\nType (book/video/electronics): ");
+            String type = sc.nextLine();
+
+            System.out.print("Title: ");
+            String title = sc.nextLine();
+
+            System.out.print("Loan days: ");
+            int loanDays = sc.nextInt();
+            sc.nextLine();
+
+            switch (type.toLowerCase()) {
+                case "book":
+                    techLab[i] = new Book(title, loanDays);
+                    break;
+                case "video":
+                    techLab[i] = new Video(title, loanDays);
+                    break;
+                case "electronic":
+                    techLab[i] = new Electronic(title, loanDays);
+                    break;
+                default:
+                    System.out.println("Unknown type, defaulting to Fruit.");
+            }
+
+        }
+
+    }
+
+
+
+    //Print all items
+    public static void printItem(Item[] techLab) {
+
+        for (int i = 0; i < techLab.length; i++) {
+            System.out.println(techLab[i]);
+        }
+    }
+
+    //Sort by title (Alphabetical A-Z)
+    public static void sortByName(Item[] techLab) {
+
+        // Bubble sort using nested loops
+        for (int i = 0; i < techLab.length - 1; i++) {
+
+            for (int j = 0; j < techLab.length - 1 - i; j++) {
+
+                // compareTo compares Strings alphabetically
+                if (techLab[j].getTitle()
+                        .compareTo(techLab[j + 1].getTitle()) > 0) {
+
+                    // Swap objects
+                    Item temp = techLab[j];
+                    techLab[j] = techLab[j + 1];
+                    techLab[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
 }
+
+
+
